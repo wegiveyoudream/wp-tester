e400fc9c7d29ef8a53cf883eb2d4aa83498db245
we.give.you.dream <we.give.you.dream@gmail.com>
2021-07-07 09:07:33 +0900

    Added ElapsedTimer

--- src/routes/UploadFile.js
+++ src/routes/UploadFile.js
@@ -1,6 +1,6 @@
-import { UploadOutlined } from "@ant-design/icons";
-import { Button, Alert, Select, Upload, Tabs } from "antd";
-import { authService, database, databaseOther, storageOther } from "fbase";
+import { UploadOutlined, LoadingOutlined } from "@ant-design/icons";
+import { Button, Alert, Select, Upload, Tabs, Row, Col } from "antd";
+import { authService, databaseOther, storageOther } from "fbase";
 import React, { useEffect, useState } from "react";
 import moment from "moment";
 import {} from "./UploadFile.css";
@@ -8,10 +8,26 @@ import { standards, seriesList } from "data";
 
 const { TabPane } = Tabs;
 
+const ElapsedTimer = ({ onElapse }) => {
+  const [secondsElapsed, setSecondsElapsed] = useState(0);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      const seconds = secondsElapsed + 1;
+      setSecondsElapsed(seconds);
+      onElapse(seconds);
+    }, 1000);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [onElapse, secondsElapsed]);
+
+  return <></>;
+};
+
 const UploadFile = () => {
-  const [wpFileUserRoot] = useState(
-    `WpFile/${process.env.REACT_APP_DEFAULT_SERIES_SEQ}/User/`
-  );
+  const wpFileUserRoot = `WpFile/${process.env.REACT_APP_DEFAULT_SERIES_SEQ}/User/`;
   // To prevent duplicated upload
   const [fileObj, setFileObj] = useState(null);
   const [seriesSeq, setSeriesSeq] = useState(
@@ -23,6 +39,7 @@ const UploadFile = () => {
   const [resultJson, setResultJson] = useState({});
   // {Point:-208.0,Groups:[{Name:"Input",Description:"입력",Point:172.0,Content:"xxx",Items:[{Name:"DictationChar",Description:"입력(글자)",Point:348.0,Content:"xxx",},{Name:"DictationWord",Description:"입력(단어)",Point:132.0,Content:"xxx",},{Name:"Rectangle",Description:"글상자",Point:11.0,Content:"xxx",},{Name:"Title1",Description:"제목(1)",Point:15.0,Content:"xxx",},{Name:"Title2",Description:"제목(2)",Point:0.0,Content:"xxx",},{Name:"Field",Description:"누름틀",Point:0.0,Content:"xxx",},{Name:"Bookmark",Description:"책갈피",Point:0.0,Content:"xxx",},{Name:"Picture",Description:"그림",Point:3.0,Content:"xxx",},{Name:"FootNote",Description:"각주",Point:6.0,Content:"xxx",},{Name:"Hyperlink",Description:"하이퍼링크",Point:5.0,Content:"xxx",},],},{Name:"Shape",Description:"모양",Point:48.0,Content:"xxx",Items:[{Name:"PageDef",Description:"편집 용지",Point:0.0,Content:"xxx",},{Name:"ColDef",Description:"단 설정",Point:0.0,Content:"xxx",},{Name:"PageBorder",Description:"쪽 테두리",Point:3.0,Content:"xxx",},{Name:"ParagraphAlign",Description:"문단 정렬",Point:0.0,Content:"xxx",},{Name:"FirstChar",Description:"문단 첫 글자 장식",Point:9.0,Content:"xxx",},{Name:"NumberHeadingType",Description:"문단 번호",Point:0.0,Content:"xxx",},{Name:"Style",Description:"스타일",Point:15.0,Content:"xxx",},{Name:"Header",Description:"머리말",Point:9.0,Content:"xxx",},{Name:"Footer",Description:"꼬리말",Point:9.0,Content:"xxx",},{Name:"PageNum",Description:"쪽 번호",Point:3.0,Content:"xxx",},],},{Name:"Table",Description:"표",Point:83.0,Content:"xxx",Items:[{Name:"Table",Description:"표",Point:71.0,Content:"xxx",},{Name:"BlockFormula",Description:"블록 계산식",Point:9.0,Content:"xxx",},{Name:"TableCaption",Description:"캡션",Point:3.0,Content:"xxx",},],},{Name:"Chart",Description:"차트",Point:5.0,Content:"xxx",Items:[{Name:"Chart",Description:"차트",Point:5.0,Content:"xxx",},],},],}
   const [message, setMessage] = useState("");
+  const [secondsElapsed, setSecondsElapsed] = useState(0);
 
   const uploadProps = {
     beforeUpload: (file) => {
@@ -116,12 +133,25 @@ const UploadFile = () => {
       });
   };
 
+  const deleteFile = (fileName) => {
+    storageOther
+      .ref(wpFileUserRoot)
+      .child(fileName)
+      .delete()
+      .then(() => {
+        console.log(`${fileName} deleted.`);
+      })
+      .catch((error) => {
+        console.log(`${fileName}:`, error);
+      });
+  };
+
   useEffect(() => {
     const completedRef = databaseOther.ref(
       `WpDb/${seriesSeq}/ListCompleted/` + authService.currentUser.uid
     );
     completedRef.on("value", (snapshot) => {
-      // FileNameStandard, FileName, InsertTime
+      console.log("snapshot", snapshot);
       const data = snapshot.val();
       if (data != null) {
         const list = Object.values(data);
@@ -138,6 +168,7 @@ const UploadFile = () => {
           setResultJson({});
         }
 
+        deleteFile(item.FileName);
         completedRef.remove();
       }
     });
@@ -170,14 +201,12 @@ const UploadFile = () => {
   }
 
   function getTabText(Description, Point) {
-    let text = Description;
-    let className = "wp_value";
-    if (Point !== 0) {
-      text += ` <${Point}>`;
-      className = "wp_value_wrong";
+    if (Point === 0) {
+      return Description;
+    } else {
+      const text = `${Description} <${Point}>`;
+      return <span className="wp_value_wrong">{text}</span>;
     }
-
-    return <span className={className}>{text}</span>;
   }
 
   return (
@@ -209,8 +238,16 @@ const UploadFile = () => {
           </Button>
         </div>
         <div className="tab-area">
-          {message && <Alert message={message} />}
-          {resultJson && (
+          {message && (
+            <>
+              <ElapsedTimer
+                onElapse={(seconds) => setSecondsElapsed(seconds)}
+              />
+              <Alert message={`${secondsElapsed} ${message}`} />
+            </>
+          )}
+
+          {resultJson?.Point && (
             <h3>
               Point:
               <span
